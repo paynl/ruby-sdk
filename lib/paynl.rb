35da4b793d90ce084304c6f6625d137ab4c66d30
Sebastian Berm <sebastian@sebsoft.nl>
2016-01-15 23:27:52 +0100

    Added config class to do something.

--- lib/paynl.rb
+++ lib/paynl.rb
@@ -1,5 +1,10 @@
-require "paynl/version"
+require 'yaml'
+require 'paynl/version'
+require 'paynl/config'
 
 module Paynl
   # Your code goes here...
 end
+
+# puts Paynl::Config::getApiVersion
+# puts Paynl::Config::getApiUrl('blap')
---
+++ lib/paynl/config.rb
@@ -0,0 +1,47 @@
+module Paynl
+  class Config
+    @@apiToken=""
+    @@serviceId=""
+    @@apiBase="https://rest-api.pay.nl"
+    @@apiVersion=5
+
+    # getApiToken - Retrieves Pay.nl api token
+    def self.getApiToken
+      return @@apiToken
+    end
+
+    # setApiToken - Sets Pay.nl api token
+    def self.setApiToken(apiToken)
+      @@apiToken = apiToken
+    end
+
+    # getServiceId - Retrieves Pay.nl Service Id
+    def self.getServiceId
+      return @@serviceId
+    end
+
+    # setServiceId - Set the Pay.nl Service Id
+    def self.setServiceId(serviceId)
+      @@serviceId = serviceId
+    end
+
+    # getApiVersion - Retrieves Pay.nl Api version
+    def self.getApiVersion
+      return @@apiVersion
+    end
+
+    # setApiVersion - Set the Pay.nl Api version
+    def self.setApiVersion(apiVersion)
+      @@apiVersion = apiVersion
+    end
+
+    # getApiUrl - Get constructed API Url for the given endpoint
+    def self.getApiUrl(endpoint, version = nil)
+      unless !version.nil?
+        version = @@apiVersion
+      end
+
+      return @@apiBase + '/v' + version.to_s + '/' + endpoint + '/json'
+    end
+  end
+end
